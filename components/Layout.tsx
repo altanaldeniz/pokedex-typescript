29ad2f58c39f12ce1c696f645eb23a6c7e86c6ae
altan <altanaldeniz@gmail.com>
2022-02-06 06:41:07 +0300

    Added footer

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Link from 'next/link';
 import { ReactNode } from 'react';
 
 type Props = {
@@ -8,14 +9,33 @@ type Props = {
 
 export default function Layout({ children, title }: Props) {
   return (
-    <div className="bg-gray-300">
+    <div className="flex min-h-screen flex-col bg-gray-300">
       <Head>
         <title>{title}</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="container mx-auto min-h-screen max-w-xl pt-8">
+
+      <header className="border-b-2 border-black py-5 text-center text-4xl">
+        <Link href={`/`}>
+          <span className="cursor-pointer border-2 border-black p-4">
+            Pokedex
+          </span>
+        </Link>
+      </header>
+      <main className="container mx-auto  max-w-xl flex-grow pt-2">
         {children}
       </main>
+      <footer className="flex h-10 w-full items-center justify-center space-x-2 border-t-2 border-black">
+        <a
+          className="hover:underline"
+          href="https://github.com/altanaldeniz"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Source
+        </a>
+        <span>Altan Aldeniz</span>
+      </footer>
     </div>
   );
 }

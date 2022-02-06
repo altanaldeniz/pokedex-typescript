3ed6f886d2b84c0db36b62167ef41da524c6150e
altan <altanaldeniz@gmail.com>
2022-02-06 06:40:09 +0300

    Added new types for pokemon detail

--- typings.d.ts
+++ typings.d.ts
@@ -3,3 +3,24 @@ export type Pokemon = {
   name: string;
   url: string;
 };
+
+export type Ability = {
+  name: string;
+};
+
+export type Type = {
+  type: {
+    name: string;
+  };
+};
+
+export type PokemonDetail = {
+  abilities: Ability[];
+  base_experience: number;
+  height: number;
+  image: string;
+  id: number;
+  name: string;
+  types: Type[];
+  weight: number;
+};
